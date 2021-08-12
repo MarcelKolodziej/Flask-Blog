4d8ab58bc20b165fed3d873da642318455543267
kolodziejmarcel1 <kolodziejmarcel1@gmail.com>
2021-08-12 13:23:53 +0200

    Database models done, DB works

--- website/__init__.py
+++ website/__init__.py
@@ -15,9 +15,26 @@ def create_app():
     from .views import views
     from .auth import auth
 
-    app.register_blueprint(views, url_prefix="/") 
+    app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    from .models import User
+
+    """Before we created db we need to import all data from models"""
+    createdatabase(app)
+
+    login_menager = LoginManager()
+    login_menager.login_view = "auth.login"
+    login_menager.init_app(app)
+
+    @login_menager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
-def create
+def createdatabase(app):
+    """Checking if DB exist, if not create one, not to do in production"""
+    if not path.exists("website/"+ DB_NAME):
+        db.create_all(app=app)
+        print("DB created!")
